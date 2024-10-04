66f28bddb3b0de563b1cf23cf0ecbab6ff7ac462
Jylan Mumtaza <jylan.mum@gmail.com>
2024-10-05 06:03:20 +0700

    Add files via upload

---
+++ 2D_menjelajah hutan.py
@@ -0,0 +1,33 @@
+r, c, m=map(int,input().split())
+hutan = []
+pos = [0,0]
+
+for _ in range(r):
+    hutan.append(list(map(int,input().split())))
+
+move = input()
+emas = hutan[pos[0]][pos[1]]    
+valid=True
+for gerak in move:
+    if gerak == 'R'and pos[1]<c-1:
+        emas += 3
+        pos[1] += 1
+    elif gerak == 'L' and pos[1]>0:
+        emas -= 2
+        pos[1] -= 1    
+    elif gerak == 'U' and pos[0]>0:
+        emas += 3
+        pos[0] -= 1
+    elif gerak == 'D'and pos[0]<r-1:
+        emas -= 2
+        pos[0] += 1
+    else:
+        valid=False
+        break
+    emas += hutan[pos[0]][pos[1]]
+
+if not valid or len(move) != m:
+    print(emas)
+    print("gerakanmu salah bung!")
+else:
+    print(f"total emas yang didapat adalah {emas}")
